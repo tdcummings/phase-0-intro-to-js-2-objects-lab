7425b1791d78a55e72a4ff55f651efefd2a7129d
Taylor Cummings <tdcummings5@gmail.com>
2022-11-13 16:20:42 -0600

    commit tree f3655827ea33311422aea7228acd217b7a60bf1d

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+let employee={
+    name:"John",
+    streetAddress:"532"
+}
+function updateEmployeeWithKeyAndValue(object, key, value) {
+let copy= Object.assign({},object, {[key]: value}) 
+console.log(copy)
+return copy 
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value; 
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    let copy= {...employee }
+    delete copy[key] 
+    return copy 
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
